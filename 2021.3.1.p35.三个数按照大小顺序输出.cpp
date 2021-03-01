a636eccc67543e3acdb0720cf7321048f95e5456
yolo <77111763+yoloeww@users.noreply.github.com>
2021-03-01 23:18:04 +0800

    Add files via upload

---
+++ 2021.3.1.p35.三个数按照大小顺序输出.cpp
@@ -0,0 +1,22 @@
+# include<stdio.h>
+int main(void)
+{
+	int a[3]={2,23,6};
+	int i,j,k;
+	for(i=0;i<2;i++)
+	{
+     	for(j=0;j<2-i;j++)
+        {
+		  if(a[j]<a[j+1])
+		  {
+		      k=a[j+1];
+			  a[j+1]=a[j];
+			  a[j]=k;
+		  }
+		}
+	}
+	for(j=0;j<3;j++)
+	printf("%d",a[j]);
+
+return 0;
+}
