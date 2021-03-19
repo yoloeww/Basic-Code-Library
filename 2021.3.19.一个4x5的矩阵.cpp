1ebe415bba7516d558c5e536644c02a2288bf2e1
yolo <77111763+yoloeww@users.noreply.github.com>
2021-03-19 23:04:53 +0800

    Add files via upload

---
+++ 2021.3.19.一个4x5的矩阵.cpp
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main(void)
+{
+   int i,j,n=0;
+   for(i=1;i<=4;i++)
+   {
+	   for(j=1;j<=5;j++,n++)
+	   {
+		   if(n%5==0)
+			   putchar('\n');
+		   printf("%5d",i*j);
+
+	   }
+   }
+   printf("\n");
+	     return 0;
+}
