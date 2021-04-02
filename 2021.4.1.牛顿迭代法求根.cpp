d2c905f08f23bd6fca30def8059c2cc7a6881944
yolo <77111763+yoloeww@users.noreply.github.com>
2021-04-02 23:13:31 +0800

    Add files via upload

---
+++ 2021.4.1.牛顿迭代法求根.cpp
@@ -0,0 +1,17 @@
+# include<stdio.h>
+# include<math.h>
+int main(void)
+{
+  float a,x0,x1;
+  printf("input a positive number:\n");
+  scanf("%f",&a);
+  x0=a/2;
+  x1=(x0+a/x0)/2;
+  do
+  {
+    x0=x1;
+	x1=(x0+a/x0)/2;
+  }while(fabs(x0-x1)>=1e-5);
+  printf("the square root of %5.2f is %8.5f\n",a,x1);
+  return 0;
+}
