a28eb68efb2d3920916c9f3f33bc1473df3c3bd0
yolo <77111763+yoloeww@users.noreply.github.com>
2021-04-11 00:35:28 +0800

    Update 2021.4.10.一元二次方程的根.cpp

--- 2021.4.10.一元二次方程的根.cpp
+++ 2021.4.10.一元二次方程的根.cpp
@@ -2,5 +2,31 @@
 # include<math.h>
 int main(void)
 {
+  double a,b,c,disc,x1,x2,realpart,imagpart;
+  printf("input a,b,c:\n");
+  scanf("%lf%lf%lf",&a,&b,&c);
+  if(fabs(a)<=1e-6)
+  printf("The equation is not a quadratic\n");
+  else
+  {
+	  disc=b*b-4*a*c;
+	  if(fabs(disc)<=1e-6)
+	  printf("the equation has two equal roots:%8.4lf\n",-b/(2*a));
+	  else
+		  if(disc>1e-6)
+		  {
+		    x1=(-b+sqrt(disc))/(2*a);
+	        x2=(-b-sqrt(disc))/(2*a);
+			printf("he equation has two real roots:%8.4lf,%8.4lf\n",x1,x2);
+		  }
+		  else
+		  {
+		   realpart=-b/(2*a);
+		   imagpart=sqrt(-disc)/(2*a);
+		   printf("the equation has two complex roots:\n");
+		   printf("%8.4lf+%8.4lfi\n",realpart,imagpart);
+		   printf("%8.4lf-%8.4lfi\n",realpart,imagpart);
+		  }
+}
   return 0;
 }
