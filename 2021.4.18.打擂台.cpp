95222e4c14e01a478e9d08532e8f5bdf3f5a4b2a
yolo <77111763+yoloeww@users.noreply.github.com>
2021-04-18 23:51:49 +0800

    Update 2021.4.18.打擂台.cpp

--- 2021.4.18.打擂台.cpp
+++ 2021.4.18.打擂台.cpp
@@ -1,4 +1,25 @@
-# include<stdio.h>
-int main(id)ads
+# include<stdio.h># include<stdio.h>
+int main(void)
 {
-return 0;}
+   //	int a[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+	int a[3][4];
+	int i,j,t=0,max=0,row=0,column=0; 
+	printf("input number of array:\n");
+	for(i=0;i<3;i++)
+		for(j=0;j<4;j++)	
+			scanf("%d",&a[i][j]);
+	max=a[0][0];
+	for(i=0;i<3;i++)
+		for(j=0;j<4;j++)	for(i=0;i<3;i++)
+		for(j=0;j<4;j++)
+			if(a[i][j]>max)
+			{
+			    t=a[i][j];
+				a[i][j]=max;
+				max=t;
+				row=i;
+				column=j;
+			}
+			printf("the large number is%d\nis %d row\nis %d column",max,row,column);
+			printf("\n");
+    return 0;}
