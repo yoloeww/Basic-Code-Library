1a93a108c29ff15570b1642bc279d9ebdd37e40f
yolo <77111763+yoloeww@users.noreply.github.com>
2021-04-26 23:48:02 +0800

    Add files via upload

---
+++ 2021.4.26.逆序 .cpp
@@ -0,0 +1,37 @@
+/*# include<stdio.h>
+int main(void)
+{
+	int a[5]={8,6,5,4,1},i,j,t=0,n=5;
+	for(i=0;i<n/2;i++)
+	{
+		t=a[i];
+		a[i]=a[n-i-1];
+		a[n-i-1]=t;
+
+	}
+	for(i=0;i<n;i++);
+	printf("%d\n",a[i]);
+	return 0;
+}*/
+#include<stdio.h>
+
+int  main()
+{
+    int a[5]={8,6,5,4,1},i,n=5,temp;
+
+    for(i=0;i<n/2;i++)
+    {
+        temp=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=temp;          
+
+
+    }                 
+
+   for(i=0;i<n;i++)
+   {
+      printf("%d\n",a[i]);        
+
+   }         
+return 0;
+}
