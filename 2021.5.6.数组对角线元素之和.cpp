61512934979409cacd7c911d7736a06cba27220d
yolo <77111763+yoloeww@users.noreply.github.com>
2021-05-06 22:42:45 +0800

    Add files via upload

---
+++ 2021.5.6.数组对角线元素之和.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main(void)
+{
+	int a[3][3]={1,2,3,
+	         	 4,5,6,
+		         7,8,9};
+	int i,j,sum=0;
+	for(i=0;i<3;i++)
+		sum+=a[i][i];
+	printf("sum=%d\n",sum);
+		return 0;
+}
