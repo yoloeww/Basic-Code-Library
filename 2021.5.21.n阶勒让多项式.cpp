860b95be28c057dbd1ed7877e999d2786bc28f49
yolo <77111763+yoloeww@users.noreply.github.com>
2021-05-22 21:57:54 +0800

    Update 2021.5.21.n阶勒让多项式.cpp

--- 2021.5.21.n阶勒让多项式.cpp
+++ 2021.5.21.n阶勒让多项式.cpp
@@ -1,8 +1,20 @@
 # include<stdio.h>
-void lerang (int a)
+float p(int n,int x)
 {
+   if(n==0)
+	   return 1;
+   else if(n==1)
+	   return (x);
+   else
+       return (2*n)-1*x*p((n-1),x)-(n-1)*p(n-2,x)/n;
 }
 int main(void)
 {
+	int x,n;
+	printf("input x and n:\n");
+	scanf("%d",&x);
+	scanf("%d",&n);
+	printf("p=%f",p(n,x));
+
 return 0;
 }
