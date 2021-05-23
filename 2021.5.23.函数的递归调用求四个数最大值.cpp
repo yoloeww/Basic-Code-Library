ed23bbcba7d04cd698b35067300af33334bf211b
yolo <77111763+yoloeww@users.noreply.github.com>
2021-05-23 21:10:50 +0800

    Add files via upload

---
+++ 2021.5.23.函数的递归调用求四个数最大值.cpp
@@ -0,0 +1,28 @@
+# include<stdio.h>
+int max4(int a,int b,int c,int d)
+{
+  int max2(int a,int b);
+  int m;
+  m=max2(a,b);
+  m=max2(m,c);
+  m=max2(m,d);
+  return m;
+}
+int max2(int a,int b)
+{
+  if(a>b)
+	  return a;
+  else
+	  return b;
+}
+int main(void)
+{
+  int a,b,c,d,max;
+  printf("Please input four numbers:\n");
+  scanf("%d%d%d%d",&a,&b,&c,&d);
+  max=max4(a,b,c,d);
+  printf("the max number =%d\n",max);
+  return 0;
+
+
+}
