9adfe5b7aaa84cd6bb746794d02d63ec3035240c
yolo <77111763+yoloeww@users.noreply.github.com>
2021-06-07 22:42:20 +0800

    Update 2021.6.6.三个字符串由小到大输出指针.cpp

--- 2021.6.6.三个字符串由小到大输出指针.cpp
+++ 2021.6.6.三个字符串由小到大输出指针.cpp
@@ -1,12 +1,23 @@
 # include<stdio.h>
-void order(char *p1,char *p2,char *p3)
+# include<string.h>
+void swap(char *p1,char *p2)
 {
-
-   
-
+       char p[20]="0";
+	   p[20]=*p2;
+	   *p2=*p1;
+	   *p1=p[20];
 }
 int main(void)
 {
-	char s1="adasdad",s2="asdasf",s3="kkksd";
+	char s1[20],s2[20],s3[20];
+	printf("input three chars:\n");
+	gets(s1);
+	gets(s2);
+	gets(s3);
+	if(strcmp(s1,s2)>0) swap(s1,s2);
+		if(strcmp(s1,s3)>0) swap(s1,s3);
+			if(strcmp(s2,s3)>0) swap(s2,s3);
+	printf("the new order is:\n");
+	printf("%s\n%s\n%s\n",s1,s2,s3);
 	return 0;
 }
